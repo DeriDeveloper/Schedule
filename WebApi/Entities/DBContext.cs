20a091b73807e1b4705bfac1ab9237ec3d24fa0c
Alexandr <dorockon2003@gmail.com>
2023-07-10 15:38:16 +0300

    add entity token user

--- WebApi/Entities/DBContext.cs
+++ WebApi/Entities/DBContext.cs
@@ -17,15 +17,13 @@ public partial class DBContext : DbContext
 
     public virtual DbSet<User> Users { get; set; }
 
+    public virtual DbSet<UserAccessToken> UserAccessTokens { get; set; }
+
     public virtual DbSet<UserRole> UserRoles { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer(
-            new ConfigurationBuilder()
-    .SetBasePath(Directory.GetCurrentDirectory())
-    .AddJsonFile("appsettings.json")
-    .Build().GetConnectionString("DBConnectionString"));
+        => optionsBuilder.UseSqlServer(Program.config.GetConnectionString("DBConnectionString"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -37,6 +35,16 @@ public partial class DBContext : DbContext
                 .HasForeignKey(d => d.UserRoleId)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK_Users_UserRoles");
+        });
+
+        modelBuilder.Entity<UserAccessToken>(entity =>
+        {
+            entity.HasNoKey();
+
+            entity.HasOne(d => d.User).WithMany()
+                .HasForeignKey(d => d.UserId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_UserAccessTokens_Users");
         });
 
         OnModelCreatingPartial(modelBuilder);
---
+++ WebApi/Entities/UserAccessToken.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApi.Entities;
+
+public partial class UserAccessToken
+{
+    public int UserId { get; set; }
+
+    public string Token { get; set; } = null!;
+
+    public virtual User User { get; set; } = null!;
+}
